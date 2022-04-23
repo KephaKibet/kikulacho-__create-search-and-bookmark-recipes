89259e9bc6b8c48063a96b62f2c046f8e21dd15b
Kepha Kibet <kibet.kepha@live.com>
2022-04-23 10:51:59 +0300

    helpers and configuration files

--- src/js/model.js
+++ src/js/model.js
@@ -1,14 +1,17 @@
+import { async } from 'regenerator-runtime';
+import { API_URL } from './config.js'
+import { getJSON } from './helpers.js'
+
+
 export const state = {
   recipe:{}
 }
  
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-    const data = await res.json()
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`)
+    const data = await getJSON(`${API_URL}/${id}`);
+    return data
+   
     console.log(res, data);
     const { recipe } = data.data;
     state.recipe = {
@@ -24,6 +27,6 @@ export const loadRecipe = async function (id) {
     console.log(state.recipe);
   }
   catch (err) {
-    alert(err)
+   console.error(`${err} 😫`)
   }
 };
